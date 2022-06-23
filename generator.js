a799a32923b27addf2c1134eb38f833c533bd89a
Jared Armstrong <97423883+jared-armstrong@users.noreply.github.com>
2022-06-23 08:26:57 -0400

    Update generator.js

--- generator.js
+++ generator.js
@@ -44,7 +44,18 @@ initializeClock('clockdiv', deadline);
 
 
 
-let questionPool = ["What color is nothing?", "Do you eat rice with a spoon or a fork?", "Is cereal a soup?", "Are boneless chicken wings just chicken nuggets in disguise?", "Is a thumb a finger?", "If you are at a restaurant and your waiter does not come back, are you the waiter?", "If you clean out a vacuum cleaner, does that make you the vacuum cleaner?", "Should toilet paper hang over or under the roll?", "What is the right way to pronounce gif?", "Are blondes really not as smart as brunettes or redheads?", "Are introverts too quiet, or are extroverts too loud?", "If you had the option, would you choose to live forever?", "Would you rather travel 100 years forward or 100 years backward?", "Would you rather fight one horse sized duck or 100 duck sized horses?", "Are ghosts real?", "Is life easier when you are beautiful?", "Are Santa’s elves compensated fairly?", "Does the person in the middle seat on an airplane get both armrests?", "Does pineapple belong on pizza?", "Would you rather have New York style pizza or Chicago style pizza?", "Are men with beards more attractive than clean shaven men?", "Should robots have faces?", "Which is better, Marvel or DC?", "Are the Beatles overrated?", "Are cats evil?", "If an ambulance is on its way to save someone and knocks down a person, should it stop to help them?", "Are birds real?", "Would you rather sit next to, or across from your significant other at a restaurant", "Do you like working from home, or prefer to go to a coffee shop/other co-working spaces?", "If you won the lottery, would you spend the money on yourself, or others?", "Do you prefer taking notes on your computer or on paper?", "Would you rather show up to a party early or late?"];
+let questionPool = ["What color is nothing?", "Do you eat rice with a spoon or a fork?", "Is cereal a soup?", 
+                    "Are boneless chicken wings just chicken nuggets in disguise?", "Is a thumb a finger?", "If you are at a restaurant and your waiter does not come back, are you the waiter?", 
+                    "If you clean out a vacuum cleaner, does that make you the vacuum cleaner?", "Should toilet paper hang over or under the roll?", 
+                    "What is the right way to pronounce gif?", "Are blondes really not as smart as brunettes or redheads?", "Are introverts too quiet, or are extroverts too loud?", 
+                    "If you had the option, would you choose to live forever?", "Would you rather travel 100 years forward or 100 years backward?", 
+                    "Would you rather fight one horse sized duck or 100 duck sized horses?", "Are ghosts real?", "Is life easier when you are beautiful?", 
+                    "Are Santa’s elves compensated fairly?", "Does the person in the middle seat on an airplane get both armrests?", "Does pineapple belong on pizza?", 
+                    "Would you rather have New York style pizza or Chicago style pizza?", "Are men with beards more attractive than clean shaven men?", "Should robots have faces?", 
+                    "Which is better, Marvel or DC?", "Are the Beatles overrated?", "Are cats evil?", "If an ambulance is on its way to save someone and knocks down a person, should it stop to help them?", 
+                    "Are birds real?", "Would you rather sit next to, or across from your significant other at a restaurant", "Do you like working from home, or prefer to go to a coffee shop/other co-working spaces?", 
+                    "If you won the lottery, would you spend the money on yourself, or others?", "Do you prefer taking notes on your computer or on paper?", 
+                    "Would you rather show up to a party early or late?"];
 
 function questionGenerator() {
   let display = document.querySelector("#text");
@@ -58,20 +69,3 @@ function questionGenerator() {
   questionPool.splice(index, 1);
   console.log(questionPool);
 }
-
-/*let testPool = ["Hello", "Another string", "Is this gonna work?", "One more string", "Wooooo"];
-
-function test() {
-  let show = document.querySelector("#text");
-  show.style.opacity="0";
-  let randomTest = testPool[Math.floor(Math.random() * testPool.length)];
-  show.style.opacity = '1';
-  show.innerHTML = randomTest;
-  console.log(randomTest);
-  
-  var index = testPool.indexOf(randomTest);
-  testPool.splice(index, 1);
-  console.log(testPool);
-  
-  
-}*/
